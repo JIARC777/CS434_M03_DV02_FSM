f4cefb0b7302fe5697ac661b9b5d2d3dd1f11821
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-02-20 20:52:03 -0700

    Changed Projects and Added Build

---
+++ FSM/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public IPlayerState currentState;
+    public float jumpHeight = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = new Standing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.Execute(this);
+    }
+}
